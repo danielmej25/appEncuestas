06b9e130779610cd51d207a7626033e671e23337
carlosrendon@unicauca.edu.co <carlosrendon@unicauca.edu.co>
2020-02-12 16:05:25 -0500

    base para seed

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
